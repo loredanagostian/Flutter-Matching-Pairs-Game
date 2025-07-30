83c8c3e79c31779d37153233be008609926ab150
Lore Gostian <loredana.gostian@cobaltsign.com>
2025-07-31 00:36:16 +0300

    created initial grid mockup and card_item model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:matching_pairs_game/models/card_item.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,46 +10,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Matching Pairs Game'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,69 +32,136 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _time = 0;
+  Timer? _timer;
+  final List<CardItem> _cards = [
+    CardItem(flippedLabel: '🔴'),
+    CardItem(flippedLabel: '🔴'),
+    CardItem(flippedLabel: '🟡'),
+    CardItem(flippedLabel: '🟡'),
+    CardItem(flippedLabel: '🟣'),
+    CardItem(flippedLabel: '🟣'),
+    CardItem(flippedLabel: '🔵'),
+    CardItem(flippedLabel: '🔵'),
+  ]..shuffle();
+  CardItem? selectedCard;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {
+        _time++;
+      });
     });
+  }
+
+  void _checkGameFinished() {
+    final allMatched = _cards.every((card) => card.state == CardState.matched);
+    if (allMatched) {
+      _timer?.cancel();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Level: Easy',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: Text(
+                'Time: $_time seconds',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 3,
+                childAspectRatio: 3 / 4,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                children: List.generate(_cards.length, (index) {
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        final card = _cards[index];
+                        if (card.state == CardState.unflipped) {
+                          card.setStateTo(CardState.flipped);
+                          if (selectedCard == null) {
+                            selectedCard = card;
+                          } else {
+                            if (selectedCard!.flippedLabel ==
+                                    card.flippedLabel &&
+                                selectedCard != card) {
+                              selectedCard!.setStateTo(CardState.matched);
+                              card.setStateTo(CardState.matched);
+                              selectedCard = null;
+                              _checkGameFinished();
+                            } else {
+                              Future.delayed(const Duration(seconds: 2), () {
+                                setState(() {
+                                  selectedCard!.setStateTo(CardState.unflipped);
+                                  card.setStateTo(CardState.unflipped);
+                                  selectedCard = null;
+                                });
+                              });
+                            }
+                          }
+                        }
+                      });
+                    },
+                    child: Card(
+                      elevation: 8,
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      color: _cards[index].color,
+                      shadowColor: Colors.transparent,
+                      child: Center(
+                        child: Text(
+                          _cards[index].displayLabel,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/models/card_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+enum CardState { unflipped, flipped, matched }
+
+class CardItem {
+  final String flippedLabel;
+  String unflippedLabel;
+  Color color;
+  CardState state;
+  String get displayLabel =>
+      state == CardState.flipped ? flippedLabel : unflippedLabel;
+
+  CardItem({
+    required this.flippedLabel,
+    this.unflippedLabel = "?",
+    this.color = Colors.grey,
+    this.state = CardState.unflipped,
+  });
+
+  void setStateTo(CardState newState) {
+    state = newState;
+    switch (newState) {
+      case CardState.unflipped:
+        color = Colors.grey;
+        break;
+      case CardState.flipped:
+        color = Colors.lightBlue[200]!;
+        break;
+      case CardState.matched:
+        color = Colors.green;
+        break;
+    }
+  }
+}
